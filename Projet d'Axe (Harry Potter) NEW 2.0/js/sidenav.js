8e36636471a659ef85e7e9e33a800f00c40cf576
wngandre <148988341+wngandre@users.noreply.github.com>
2024-05-07 16:35:27 +0200

    Projet d'Axe (Harry Potter) NEW 2.0 directory

--- Projet d'Axe (Harry Potter) NEW 2.0/js/sidenav.js
+++
@@ -1,6 +0,0 @@
-let menuHamburger = document.querySelector(".menu-burger")
-let navLinks = document.querySelector(".header-center")
-
-menuHamburger.addEventListener('click', function() {
-navLinks.classList.toggle('mobile-menu')
-})
